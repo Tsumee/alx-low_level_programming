519816097b6156633a90e6fb7d2852e692d4246f
Tsumee <tsumeemoloisi@gmail.com>
2023-02-17 02:13:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<unistdio.h>
+#include<unistd.h>
 /**
  * main - is entry point
  *
- * Return: 1, if successful
+ * Return: 1, if succesful
  */
 int main(void)
 {
-	wite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
