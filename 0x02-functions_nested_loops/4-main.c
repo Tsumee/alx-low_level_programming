4a1ba4a5e3b68efe3827f4b36186120540a0dc96
Tsumee <tsumeemoloisi@gmail.com>
2023-02-21 16:40:01 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-isalpha.c -o 4-isalpha
+julien@ubuntu:~/0x02$ ./4-isalpha 
+1110
+julien@ubuntu:~/0x02$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x02-functions_nested_loops
+File: 4-isalpha.c
+   
+5. Sign
+mandatory
+Write a function that prints the sign of a number.
+
+Prototype: int print_sign(int n);
+Returns 1 and prints + if n is greater than zero
+Returns 0 and prints 0 if n is zero
+Returns -1 and prints - if n is less than zero
+julien@ubuntu:~/0x02$ cat 5-main.c
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
