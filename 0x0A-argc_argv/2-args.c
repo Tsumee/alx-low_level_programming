16eab5e61d402ec1bcaaad50cfa078fa05ff285e
Tsumee <tsumeemoloisi@gmail.com>
2023-03-10 06:11:47 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,47 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
-* main - a program that prints the minimum number of coins to make change
+* main - a program that prints all arguments it receives
 * @argc: the number of command-line arguments
-* @argv: an array of strings that contain the command-line arguments
-* Return: returns 0 (sucessful), otherwise returns 1 on error
+* @argv: an array of strings containing the command-line arguments
+* Return: returns 0 (success)
 */
 int main(int argc, char *argv[])
 {
-	int cents;
-	int quarters_25, dimes_10, nickels_5, coin_2, pennies_1;
-	int change;
+	int i = 0;
 
-	if (argc != 2)
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	quarters_25 = cents / 25;
-	cents = cents - (quarters_25 * 25);
-
-	dimes_10 = cents / 10;
-	cents = cents - (dimes_10 * 10);
-
-	nickels_5 = cents / 5;
-	cents = cents - (nickels_5 * 5);
-
-	coin_2 = cents / 2;
-	cents = cents - (coin_2 * 2);
-
-	pennies_1 = cents / 1;
-
-	change = quarters_25 + dimes_10 + nickels_5 + coin_2 + pennies_1;
-	printf("%d\n", change);
 	return (0);
 }
-
